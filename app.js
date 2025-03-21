a03a391d3dfd6e74bedfb56d24da201949904651
LVerdadeiro <lucas.candolo@gmail.com>
2025-03-21 13:42:21 -0300

    Correção do problema de troca de tema. Adição de telas HTML para efetuar troca de página

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-let modo_claro = localStorage.getItem("modo_claro");
-const trocaTema = document.getElementById("trocar_tema");
+const paginaAtiva = window.location.pathname;
 
-const habilitaModoClaro = () => {
-    document.body.classList.add("modo_claro");
-    localStorage.setItem("modo_claro", "ativo");
-};
+const nav_links = document.querySelectorAll("nav a").forEach(link => {
+    if(link.href.includes(`${paginaAtiva}`)){
+        link.classList.add("ativo");
+    }
+})
 
-const desabilitarModoClaro = () => {
-    document.body.classList.remove("modo_claro");
-    localStorage.removeItem("modo_claro");
-};
-
-
-if (modo_claro === "ativo") {
-    habilitaModoClaro();
-}
-
-trocaTema.addEventListener("click", () => {
-    if (localStorage.getItem("modo_claro") === "ativo") {
-        desabilitarModoClaro();
-    } else {
+document.addEventListener("DOMContentLoaded", () => {
+    let modo_claro = localStorage.getItem("modo_claro");
+    const trocaTema = document.getElementById("trocar_tema");
+    
+    const habilitaModoClaro = () => {
+        document.body.classList.add("modo_claro");
+        localStorage.setItem("modo_claro", "ativo");
+    };
+    
+    const desabilitarModoClaro = () => {
+        document.body.classList.remove("modo_claro");
+        localStorage.removeItem("modo_claro", "inativo");
+    };
+    
+    if (modo_claro === "ativo") {
         habilitaModoClaro();
     }
-});
+    
+    trocaTema.addEventListener("click", () => {
+        if (localStorage.getItem("modo_claro") === "ativo") {
+            desabilitarModoClaro();
+        } else {
+            habilitaModoClaro();
+        }
+    });
+})
